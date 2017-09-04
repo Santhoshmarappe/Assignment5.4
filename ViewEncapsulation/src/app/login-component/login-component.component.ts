5a66e60374f8e69d139a233859de15fe449c69d4
Santhoshmarappe <santhoshkumar.marappe@epsilon.com>
2017-09-04 12:23:27 +0530

    Folder Structure, Components, Styles add a ViewEncapsulation metadata in the @Component. changing the property for the ViewEncapsulation.

---
+++ ViewEncapsulation/src/app/login-component/login-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit,  ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login-component.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  styleUrls: ['./login-component.component.css']
+})
+export class LoginComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
